27679ab7e82e3a0d6b8c080f20f898164a1c3ef3
Tran Thanh Sang <ttsang93@gmail.com>
2023-11-11 14:29:28 +0700

    Video 19: DOM events

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,9 +6,23 @@ class MyComponent extends React.Component {
     address: "Hoi dan IT",
     age: "30",
   };
+  handleClick(event) {
+    console.log(">> Click me my button");
+    console.log(event);
+    console.log(this.state.name);
+  }
+  handleOnMouseOver(event) {
+    console.log(event.pageX);
+  }
   // JSX
   render() {
-    return <div>My name is {this.state.name}</div>;
+    return (
+      <div>
+        My name is {this.state.name}
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+        <button onClick={this.handleClick}>Click me</button>
+      </div>
+    );
   }
 }
 export default MyComponent;
