276726a2fdd3b04109e6b8009ea85b903c9f2247
Tran Thanh Sang <ttsang93@gmail.com>
2023-11-13 08:02:01 +0700

    Video 33: React Lifecycle - Vòng đời ứng dụng React

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./DisplayInfo.scss";
-import logo from "./../logo.svg";
+// import logo from "./../logo.svg";
 
 class DisplayInfo extends React.Component {
-  state = {
-    isShow: true,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShow: true,
+    };
+    console.log(">>> Call constructor function");
+  }
+  componentDidMount() {
+    console.log(">>> call me component did mount");
+    setTimeout(() => {
+      document.title = "Hoi dan IT";
+    }, 3000);
+  }
+  componentDidUpdate(prevProps, prevState, snapshop) {
+    console.log(">>> Call me component did update", this.props, prevProps);
+    if (this.props.listUsers !== prevProps.listUsers) {
+      if (this.props.listUsers.length === 5) {
+        alert("You got 5 users");
+      }
+    }
+  }
   handleShowHide() {
     this.setState({
       isShow: !this.state.isShow,
@@ -14,6 +32,7 @@ class DisplayInfo extends React.Component {
 
   // JSX
   render() {
+    console.log(">>> Call me render ");
     const { listUsers } = this.props;
     console.log(listUsers);
 
