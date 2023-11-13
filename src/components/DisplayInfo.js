9176f80e85fe2683b31bdaab03d08f3196cc2791
Tran Thanh Sang <ttsang93@gmail.com>
2023-11-13 09:00:53 +0700

    Video 38: useEffect Hook

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfo.scss";
 // import logo from "./../logo.svg";
 
-
-
 const DisplayInfo = (props) => {
   const [isShowUser, setIsShowUser] = useState(true);
+  const { listUsers } = props;
 
   const handleShowHide = () => {
     setIsShowUser((prev) => !prev);
   };
+  useEffect(() => {
+    setTimeout(() => {
+      document.title = "Hoi dan IT";
+    }, 3000);
+
+    if (listUsers.length === 0) {
+      alert("You deleted all users!!");
+    }
+  }, [listUsers]);
 
   return (
     <div className="display-info-container">
@@ -19,7 +27,7 @@ const DisplayInfo = (props) => {
       <br /> <br />
       <div>
         {isShowUser &&
-          props.listUsers.map((user) => {
+          listUsers.map((user) => {
             return (
               <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
                 <div> My name: {user.name}</div>
